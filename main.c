f2f160daa26c8e9396d441b49aa58ed822235154
aczech17 <adrian-czech@outlook.com>
2024-11-16 21:35:49 +0100

    cursor moving

--- main.c
+++ main.c
@@ -23,27 +23,26 @@ Screen_info get_screen_info()
     return info;
 }
 
-void print_line(const char* line, int line_size, int screen_width, int pos_x, int screen_line)
+void print_line(const char* line, int line_size, int screen_width, int cursor_x, int screen_line)
 {
-    int start_index = (pos_x / screen_width) * screen_width;
-    int end_index = start_index + screen_width;
-
-    if (end_index >= line_size)
-        end_index = line_size;
-
     char* buffer = malloc(screen_width + 1);
     memset(buffer, ' ', screen_width);
     buffer[screen_width] = 0;
 
-    if (pos_x < line_size)
-        strncpy(buffer, line + start_index, end_index - start_index);
+    int start_index = (cursor_x / screen_width) * screen_width;
+    int end_index = start_index + screen_width;
+    if (end_index >= line_size)
+        end_index = line_size;
+
+    if (start_index < line_size)
+        strncpy(buffer, line + start_index, end_index - start_index);        
     
     DWORD written;
     WriteConsoleOutputCharacterA(STD, buffer, screen_width, (COORD){0, screen_line}, &written);
     free(buffer);
 }
 
-void print_file_content(const char* content, const Screen_info* screen_info, int pos_x, int text_line)
+void print_file_content(const char* content, const Screen_info* screen_info, int cursor_x, int text_line)
 {
     int line_count = screen_info->height;
     int screen_width = screen_info->width;
@@ -64,12 +63,12 @@ void print_file_content(const char* content, const Screen_info* screen_info, int
         if (next_line == NULL)
         {
             int line_size = strlen(content);
-            print_line(content, line_size, screen_width, pos_x, screen_line);
+            print_line(content, line_size, screen_width, cursor_x, screen_line);
             break;
         }
 
         int line_size = next_line - content;
-        print_line(content, line_size, screen_width, pos_x, screen_line);
+        print_line(content, line_size, screen_width, cursor_x, screen_line);
 
         content = next_line + 1;
     }
@@ -121,18 +120,21 @@ int main(int argc, char** argv)
     Text_info* text_info = get_text_info(content);
     int line_count = text_info != NULL ? text_info->line_count : 0;
 
-
     int pos_x = 0, pos_y = 0;
-    int text_line = 0;
+    int first_text_line = 0;
     INPUT_RECORD input_record;
     DWORD events;
 
     system("cls");
     for (;;)
     {
-        
-        print_file_content(content, &screen_info, pos_x, text_line);
-        SetConsoleCursorPosition(STD, (COORD){pos_x, pos_y});
+        int text_line = first_text_line + pos_y;
+        int line_size = text_info->line_sizes[text_line];
+        if (pos_x > line_size)
+            pos_x = line_size;
+
+        print_file_content(content, &screen_info, pos_x, first_text_line);
+        SetConsoleCursorPosition(STD, (COORD){pos_x % screen_info.width, pos_y});
 
         ReadConsoleInput(GetStdHandle(STD_INPUT_HANDLE), &input_record, 1, &events);
         if (input_record.EventType == KEY_EVENT && input_record.Event.KeyEvent.bKeyDown) 
@@ -143,8 +145,8 @@ int main(int argc, char** argv)
                 {
                     if (pos_y > 0)
                         --pos_y;
-                    else if (text_line > 0)
-                        --text_line;
+                    else if (first_text_line > 0)
+                        --first_text_line;
 
                     break;
                 }
@@ -156,8 +158,8 @@ int main(int argc, char** argv)
                         ++pos_y;
                     else
                     {
-                        if (text_line < line_count - 1)
-                            ++text_line;
+                        if (first_text_line < line_count - 1)
+                            ++first_text_line;
                     }
                         
                     break;
@@ -169,8 +171,12 @@ int main(int argc, char** argv)
                     }
                     break;
                 case VK_RIGHT:
+                {
+                    if (pos_x == line_size)
+                        break;
                     pos_x++;
                     break;
+                }
                 case VK_ESCAPE:
                     goto end;
             }
