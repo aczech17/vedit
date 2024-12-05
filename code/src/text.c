c73f9e737fb0e28fc1863b2c9e5dc5cee562fc75
aczech17 <adrian-czech@outlook.com>
2024-12-05 21:27:00 +0100

    empty lines work somehow

--- code/src/text.c
+++ code/src/text.c
@@ -300,14 +300,33 @@ void join_lines(Text* text, int upper_line_number)
 
 bool save_text(const Text* text, const char* filename)
 {
-    FILE* output_file = fopen(filename, "w");
+    FILE* output_file = fopen(filename, "wb");
     if (output_file == NULL)
         return false;
 
-    for (int line_number = 0; line_number < text->line_count; ++line_number)
+    if (text->line_count == 1 && strlen(text->lines[0]) == 0) // empty file
+    {
+        fclose(output_file);
+        return true;
+    }
+
+    for (int line_number = 0; line_number < text->line_count - 2; ++line_number)
     {
         char* line = text->lines[line_number];
         fprintf(output_file, "%s\n", line);
+    }
+
+    char* penultimate_line = text->lines[text->line_count - 2];
+    char* last_line = text->lines[text->line_count - 1];
+
+    if (strlen(last_line) == 0) // The last line is empty, so the last line is endline
+    {
+        fprintf(output_file, "%s\n", penultimate_line);
+    }
+    else    // The last line is not empty, so there's no empty line at the end. Write two last lines.
+    {
+        fprintf(output_file, "%s\n", penultimate_line);
+        fprintf(output_file, "%s", last_line);
     }
 
     fclose(output_file);
